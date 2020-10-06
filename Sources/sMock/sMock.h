d36317b4d1018b04cad540496dd1f41f8c6bd9ed
chchrn <chicherin.and@gmail.com>
2020-10-07 02:12:20 +0700

    Added xcode project for carthage supports

---
+++ Sources/sMock/sMock.h
@@ -0,0 +1,18 @@
+//
+//  sMock.h
+//  sMock
+//
+//  Created by chchrn on 07.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sMock.
+FOUNDATION_EXPORT double sMockVersionNumber;
+
+//! Project version string for sMock.
+FOUNDATION_EXPORT const unsigned char sMockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sMock/PublicHeader.h>
+
+
